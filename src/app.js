925f4e872b2ed6461f04cee72b36df3be7e146e9
Parmeshwar Kumar Sahu <parmeshwarsahu17@gmail.com>
2020-05-11 22:37:04 +0530

    socket io setup

--- src/app.js
+++ src/app.js
@@ -1,15 +1,13 @@
 const express = require('express');
+const http = require('http');
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
 
+const server = http.createServer();
+
 const pub_folder = path.join(__dirname, '../public');
-
 app.use(express.static(path.join(pub_folder)));
-
-app.get('/', (req, res)=>{
-  res.sendFile(path.join(__dirname, '../public/index.htm'));
-})
 
 app.listen(port, ()=>{
   console.log(`my chat app is running on port ${port} on localhost`) ;
