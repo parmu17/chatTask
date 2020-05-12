2b75d7173c8fa94085e2996044d9d9e85fa4072f
Parmeshwar Kumar Sahu <parmeshwarsahu17@gmail.com>
2020-05-12 12:28:53 +0530

    values transfered to client

--- public/js/logic1.js
+++ public/js/logic1.js
@@ -1,6 +1,9 @@
 const socket = io();
-socket.on('pageUpdated1', ()=>{
-  console.log('the page has been update');
+
+socket.on('pageUpdated1', (val1, val2)=>{
+  console.log('the page has been updated. The values are ' + val1 + ' and ' + val2);
+  //console.log(val1);
+  //console.log(val2);
 });
 
 //console.log('from logic1 client script');
--- src/app.js
+++ src/app.js
@@ -11,9 +11,10 @@ const port = process.env.PORT || 3000;
 const pub_folder = path.join(__dirname, '../public');
 app.use(express.static(path.join(pub_folder)));
 
-io.on('connection', (socket)=>{
+var val1 = 65, val2 = 30;
+io.on('connection', (socket1)=>{
   console.log('My chatTask server got a new websocket connection..'); //upon request from client page
-  socket.emit('pageUpdated1');
+  socket1.emit('pageUpdated1', val1, val2);
 });
 
 server.listen(port, ()=>{
