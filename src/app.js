209f143357a97fe64527d9f535f206814c5de87c
Parmeshwar Kumar Sahu <parmeshwarsahu17@gmail.com>
2020-05-12 22:50:09 +0530

    broadcast feature added to let all other members inform about new joinee

--- src/app.js
+++ src/app.js
@@ -11,25 +11,16 @@ const port = process.env.PORT || 3000;
 const pub_folder = path.join(__dirname, '../public');
 app.use(express.static(path.join(pub_folder)));
 
-var msg1 = "Hello there, Most welcome in chat application";
 io.on('connection', (socket)=>{
-  console.log('My chatTask server got a new websocket connection..'); //upon request from client page
-  socket.emit('page1Updated', msg1);  //sending event
-  
-  socket.on('myevent1', (recMsg1)=>{  //receiving event
-    console.log(recMsg1);
-    var msg2 = "I received your message that says '" + recMsg1 + "'" ;
-    //io.emit('pageUpdated1', msg1); //update for all clients
-    socket.emit('event1Updated', msg2); //update only for requested client
+  console.log('new client connected')
+  socket.emit('event1', "Most Welcome.."); //to every one upon connection,( event1)
+  socket.broadcast.emit('event1', 'A new user joined our community') //for every one except the new client, (event1)
+  socket.on('event2', (msg2)=>{
+    console.log(msg2);
+    var msg3 = "your message ''" + msg2 + "' received, yours sincerely server.."
+    socket.emit('event3', msg3);  //send to the spsecific client
   })
-  
-  socket.on('myevent2', (recMsg2)=>{
-    console.log(recMsg2);
-    var msg3 =  "I received your message that says '" + recMsg2 + "'" ;
-    socket.emit('event2Updated', msg3);
-  })
-
-});
+})
 
 server.listen(port, ()=>{
   console.log(`My chatTask express server is running on localhost at port ${port}`) ;
