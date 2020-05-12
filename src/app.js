abc605132c1655dce61fe6d1b3a3ab8f3d3fab82
Parmeshwar Kumar Sahu <parmeshwarsahu17@gmail.com>
2020-05-12 18:44:29 +0530

    could sent form content using "document-querySelector-input-value concept"

--- src/app.js
+++ src/app.js
@@ -11,14 +11,24 @@ const port = process.env.PORT || 3000;
 const pub_folder = path.join(__dirname, '../public');
 app.use(express.static(path.join(pub_folder)));
 
-var val1 = 65, val2 = 30;
+var msg1 = "Hello there, Most welcome in chat application";
 io.on('connection', (socket)=>{
   console.log('My chatTask server got a new websocket connection..'); //upon request from client page
-  socket.emit('pageUpdated1', val1, val2);
-  socket.on('myevent1', ()=>{
-    val1 += 5;
-    socket.emit('pageUpdated1', val1, val2);
+  socket.emit('page1Updated', msg1);  //sending event
+  
+  socket.on('myevent1', (recMsg1)=>{  //receiving event
+    console.log(recMsg1);
+    var msg2 = "I received your message that says '" + recMsg1 + "'" ;
+    //io.emit('pageUpdated1', msg1); //update for all clients
+    socket.emit('event1Updated', msg2); //update only for requested client
   })
+  
+  socket.on('myevent2', (recMsg2)=>{
+    console.log(recMsg2);
+    var msg3 =  "I received your message that says '" + recMsg2 + "'" ;
+    socket.emit('event2Updated', msg3);
+  })
+
 });
 
 server.listen(port, ()=>{
