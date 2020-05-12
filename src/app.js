ccca22bc355de73a09169a8dac98b66b4acda657
Parmeshwar Kumar Sahu <parmeshwarsahu17@gmail.com>
2020-05-12 10:28:21 +0530

    websocket feature added successfully

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
+const path = require('path');
 const express = require('express');
 const http = require('http');
+const socketio = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 const port = process.env.PORT || 3000;
-const path = require('path');
-
-const server = http.createServer();
-
 const pub_folder = path.join(__dirname, '../public');
 app.use(express.static(path.join(pub_folder)));
 
-app.listen(port, ()=>{
-  console.log(`my chat app is running on port ${port} on localhost`) ;
-})
+io.on('connection', (socket)=>{
+  console.log('My chatTask server got a new websocket connection..'); //upon request from client page
+  socket.emit('pageUpdated1');
+});
+
+server.listen(port, ()=>{
+  console.log(`My chatTask express server is running on localhost at port ${port}`) ;
+});
